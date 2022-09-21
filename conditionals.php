247b58b1edf1aacc5690eb7e6b374a272ec21149
Alex Johnson <johnsonav1992@gmail.com>
2022-09-21 14:06:46 -0500

    conditionals

---
+++ conditionals.php
@@ -0,0 +1,19 @@
+<?php
+    $some_number = 4;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conditionals</title>
+</head>
+
+<body>
+    <h1>Here is the number we added: <?php echo $some_number; ?></h1>
+</body>
+
+</html> 
