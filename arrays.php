da721bed54ea4821daeddd00ef028743cf176ff6
Alex Johnson <johnsonav1992@gmail.com>
2022-09-21 13:58:15 -0500

    arrays

---
+++ arrays.php
@@ -0,0 +1,13 @@
+<?php
+
+$numbers = [1, 2, 3, 4];
+$fruit = array('orange', 'apple');
+
+echo $fruit[0];
+$hex = [
+    'red' => '#f00',
+    'blue' => '#0f0'
+];
+
+var_dump($hex);
+var_dump(json_encode($hex));
--- output.php
+++ output.php
@@ -6,4 +6,7 @@ $arr = [1, 2, 3];
 var_dump($my_first_variable);
 var_dump($arr);
 
-?>
+$name = 'Alex';
+$age = '30';
+
+echo "${name} is ${age} years old. Unfortunately....";
